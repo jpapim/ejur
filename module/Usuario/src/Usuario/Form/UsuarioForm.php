6dc5ca9efdd8e164a0cfbed5a46a6f548ff5256f
José Matheus <despairsray10@gmail.com>
2016-10-14 16:49:15 -0300

    Retirando menu relatorio redundante

--- module/Usuario/src/Usuario/Form/UsuarioForm.php
+++ module/Usuario/src/Usuario/Form/UsuarioForm.php
@@ -24,7 +24,7 @@ class UsuarioForm extends AbstractForm {
         $objForm->text("nm_nacionalidade")->required(false)->label("Nacionalidade");
         $objForm->combo("id_sexo", '\Sexo\Service\SexoService', 'id', 'nm_sexo')->required(false)->label("Sexo");
         $objForm->combo("id_tipo_usuario", '\TipoUsuario\Service\TipoUsuarioService', 'id', 'nm_tipo_usuario')->required(true)->label("Tipo de Usuário");
-        $objForm->combo("id_situacao_usuario", '\SituacaoUsuario\Service\SituacaoUsuarioService', 'id', 'nm_situacao_usuario')->required(true)->label("Situação do Usuário");
+        $objForm->combo("id_situacao_usuario", '\SituacaoUsuario\Service\SituacaoUsuarioService', 'id', 'nm_situacao_usuario')->required(false)->label("Situação do Usuário");
         $objForm->email("em_email")->required(true)->label("Email");
         $objForm->email("em_email_confirm")->required(true)->label("Confirme o email")
                 ->setAttribute('data-match', '#em_email')
