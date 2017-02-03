bd8d28ca8cf3728359d3ee164b9c47d5b0eecb1e
yves <yveslopo@gmail.com>
2017-02-03 12:49:41 -0200

    Módulo Prova - Correção dos erros ortográficos nos FORMs e na index-pagination, também foi corrigido o CSS que estava faltando em um dos botões.

--- module/Prova/src/Prova/Form/QuestaoManualForm.php
+++ module/Prova/src/Prova/Form/QuestaoManualForm.php
@@ -17,11 +17,11 @@ class QuestaoManualForm extends AbstractForm{
 
         $objForm->combo("id_tipo_questao", '\TipoQuestao\Service\TipoQuestaoService','id','nm_tipo_questao')->required(true)->label("Tipo");
         $objForm->combo("id_fonte_questao", '\Fonte\Service\FonteService','id','nm_fonte_questao')->required(false)->label("Fonte da Questão");
-        $objForm->select("id_materia", array(''=>'Selecione um Semestre...'))->required(false)->label("Materia");
+        $objForm->select("id_materia", array(''=>'Selecione um Semestre...'))->required(false)->label("Matéria");
         $objForm->select("id_assunto_materia", array(''=>'Selecione uma Matéria...'))->required(false)->label("Assunto");
-        $objForm->combo("id_nivel_dificuldade", '\NivelDificuldade\Service\NivelDificuldadeService','id','nm_nivel_dificuldade')->required(false)->label("Nivel de Dificuldade");
+        $objForm->combo("id_nivel_dificuldade", '\NivelDificuldade\Service\NivelDificuldadeService','id','nm_nivel_dificuldade')->required(false)->label("Nível de Dificuldade");
         $objForm->combo("id_classificacao_semestre", '\Classificacao\Service\ClassificacaoService','id','nm_classificacao_semestre')->required(false)->label("Semestre");
-        $objForm->integer("nr_questoes")->required(false)->label("Quantidade de Questoes");
+        $objForm->integer("nr_questoes")->required(false)->label("Quantidade de Questões");
 
         $this->formObject = $objForm;
     }
--- module/Prova/src/Prova/Form/VariasQuestoesAleatoriasForm.php
+++ module/Prova/src/Prova/Form/VariasQuestoesAleatoriasForm.php
@@ -17,11 +17,11 @@ class VariasQuestoesAleatoriasForm extends AbstractForm{
 
         $objForm->combo("id_tipo_questao", '\TipoQuestao\Service\TipoQuestaoService','id','nm_tipo_questao')->required(true)->label("Tipo");
         $objForm->combo("id_fonte_questao", '\Fonte\Service\FonteService','id','nm_fonte_questao')->required(true)->label("Selecionar a fonte");
-        $objForm->select("id_materia", array(''=>'Selecione um Semestre...'))->required(true)->label("Materia");
+        $objForm->select("id_materia", array(''=>'Selecione um Semestre...'))->required(true)->label("Matéria");
         $objForm->select("id_assunto_materia", array(''=>'Selecione uma Matéria...'))->required(true)->label("Assunto");
-        $objForm->combo("id_nivel_dificuldade", '\NivelDificuldade\Service\NivelDificuldadeService','id','nm_nivel_dificuldade')->required(true)->label("Nivel de Dificuldade");
+        $objForm->combo("id_nivel_dificuldade", '\NivelDificuldade\Service\NivelDificuldadeService','id','nm_nivel_dificuldade')->required(true)->label("Nível de Dificuldade");
         $objForm->combo("id_classificacao_semestre", '\Classificacao\Service\ClassificacaoService','id','nm_classificacao_semestre')->required(true)->label("Semestre");
-        $objForm->integer("nr_questoes")->required(true)->label("Quantidade de Questoes");
+        $objForm->integer("nr_questoes")->required(true)->label("Quantidade de Questões");
 
         $this->formObject = $objForm;
     }
