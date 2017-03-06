0d728d55db9235b637f4de584b10b99cf1d39cbd
Alysson Vicuña de Oliveira <alyssontkd@gmail.com>
2017-03-06 13:17:52 -0300

    [Bugfix] Corrgindo o cadastro de questões porque estava apresentando erro de carregamento dos assuntos matérias sempre que um assunto nula era encontrado

--- module/AssuntoMateria/src/AssuntoMateria/Form/AssuntoMateriaForm.php
+++ module/AssuntoMateria/src/AssuntoMateria/Form/AssuntoMateriaForm.php
@@ -14,7 +14,7 @@ class AssuntoMateriaForm extends AbstractForm{
         $objForm = new FormObject('assuntomateriaform',$this,$this->inputFilter);
         $objForm->hidden("id")->required(false)->label("Id");
         $objForm->combo("id_materia", '\Materia\Service\MateriaService', 'id', 'nm_materia')->required(true)->label("Matéria");
-        $objForm->text("nm_assunto_materia")->required(false)->label("Assunto");
+        $objForm->text("nm_assunto_materia")->required(true)->label("Assunto");
 
         $this->formObject = $objForm;
     }
