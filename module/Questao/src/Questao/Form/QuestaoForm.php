87823cebc15337690734a552dd236c7cc970506c
Raimundo de Paula <Raimundodep@gmail.com>
2017-03-07 17:01:36 -0300

    Mesclando cadastro de questoes com alternativas

--- module/Questao/src/Questao/Form/QuestaoForm.php
+++ module/Questao/src/Questao/Form/QuestaoForm.php
@@ -34,6 +34,33 @@ class QuestaoForm extends AbstractForm
 
         $objForm->hidden("id_usuario_cadastro")->required(false)->label("id_usuario_cadastro");
         $objForm->hidden("id_usuario_alteracao")->required(false)->label("id_usuario_alteracao");
+        $objForm->hidden("id_questao")->required(false)->label("Id Questao");
+        $objForm->textareaHtml("id_alternativa_questao_1")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("id_alternativa_questao_2")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("id_alternativa_questao_3")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("id_alternativa_questao_4")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("id_alternativa_questao_5")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("tx_alternativa_questao_1")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("tx_alternativa_questao_2")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("tx_alternativa_questao_3")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("tx_alternativa_questao_4")->required(false)->label("Enunciado da Alternativa");
+        $objForm->textareaHtml("tx_alternativa_questao_5")->required(false)->label("Enunciado da Alternativa");
+        $objForm->select("cs_correta", array('C' => 'Correta', 'E'=>'Errada'))->required(false)->label("Resposta");
+        $objForm->select("cs_correta_1", array('C' => 'Correta', 'E'=>'Errada'))->required(false)->label("Resposta");
+        $objForm->select("cs_correta_2", array('C' => 'Correta', 'E'=>'Errada'))->required(false)->label("Resposta");
+        $objForm->select("cs_correta_3", array('C' => 'Correta', 'E'=>'Errada'))->required(false)->label("Resposta");
+        $objForm->select("cs_correta_4", array('C' => 'Correta', 'E'=>'Errada'))->required(false)->label("Resposta");
+        $objForm->select("cs_correta_5", array('C' => 'Correta', 'E'=>'Errada'))->required(false)->label("Resposta");
+        
+        $objForm->textareaHtml("tx_justificativa")->required(false)->label("Justificativa da Alternativa");
+        $objForm->textareaHtml("tx_justificativa_1")->required(false)->label("Justificativa da Alternativa");
+        $objForm->textareaHtml("tx_justificativa_2")->required(false)->label("Justificativa da Alternativa");
+        $objForm->textareaHtml("tx_justificativa_3")->required(false)->label("Justificativa da Alternativa");
+        $objForm->textareaHtml("tx_justificativa_4")->required(false)->label("Justificativa da Alternativa");
+        $objForm->textareaHtml("tx_justificativa_5")->required(false)->label("Justificativa da Alternativa");
+        $objForm->text("tx_caminho_imagem_alternativa")->required(false)->label("Caminho da Imagem");
+        $objForm->combo("id_usuario_cadastro", '\Usuario\Service\UsuarioService', 'id', 'nm_usuario')->required(false)->label("Usuario");
+        $objForm->combo("id_usuario_alteracao", '\Usuario\Service\UsuarioService', 'id', 'nm_usuario')->required(false)->label("Usuario");
 
         $this->formObject = $objForm;
     }
