1f1aece571d114922784d3ad7905f7a00eb92e93
Alysson Vicuña de Oliveira <alyssontkd@gmail.com>
2017-03-23 22:21:50 -0300

    Criando a procedure para relacionar questoes desvinculadas das alternativas

--- data/db/cola.sql
+++ data/db/cola.sql
@@ -1,3 +1,22 @@
+###EXPORTAR WINDOWS
 mysqldump -u root -B bdejur -p > C:\xampp\htdocs\ejur\data\db\script_inicial.sql
 
+###IMPORTAR WINDOWS
 mysql -u root -p < C:\xampp\htdocs\ejur\data\db\script_inicial.sql
+
+
+###EXPORTAR LINUX
+mysqldump -u root -B bdejur -p > /var/www/html/juridico.acthosti.com.br/data/db/script_inicial.sql
+
+
+###IMPORTAR LINUX
+mysql -u root -p < /var/www/html/juridico.acthosti.com.br/data/db/script_inicial.sql
+
+
+
+
+
+mysqldump -u root -B bdejur -p > /tmp/script_inicial_backup.sql
+
+
+C:\xampp\htdocs\ejur\data\db\bkp\script_inicial_backup.sql
