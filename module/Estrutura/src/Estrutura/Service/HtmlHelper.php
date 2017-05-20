c657b5d220c5210435d7a57708500a51ebc90d21
EduFerr <eduardofgsantos@gmail.com>
2017-05-20 10:23:40 -0300

    subindo modal confirma excluir no módulo Questão

--- module/Estrutura/src/Estrutura/Service/HtmlHelper.php
+++ module/Estrutura/src/Estrutura/Service/HtmlHelper.php
@@ -2,59 +2,87 @@
 
 namespace Estrutura\Service;
 
-class HtmlHelper{
-    public static function botaoLink($url, $icone, $attributos=[], $title='Alterar'){
+class HtmlHelper
+{
+    public static function botaoLink($url, $icone, $attributos = [], $title = 'Alterar')
+    {
         $attributos = self::arrayToString($attributos);
 
-        $botao = '<a href="'.$url.'" '.$attributos.'>
-                     <button title="'. $title .'" type="button" class="btn btn-default">
-                       <span class="'.$icone.'"></span>
+        $botao = '<a href="' . $url . '" ' . $attributos . '>
+                     <button title="' . $title . '" type="button" class="btn btn-default">
+                       <span class="' . $icone . '"></span>
                      </button>
                   </a>';
 
         return $botao;
     }
 
-    public static function botaoNovo($url, $title='Novo'){
-        $attributos = ['class'=>'btn-novo btn-xs', 'id'=>'botaonovo'];
+    public static function botao($id, $icone, $attributos = [], $title = 'Alterar')
+    {
+        $attributos = self::arrayToString($attributos);
+
+        $botao = '<span ' . $attributos . ' rel="' . $id . '"> 
+                    <button title="' . $title . '" type="button" class="btn btn-default">
+                    <span class="' . $icone . '"></span> 
+                    </button>
+                    </span>';
+        return $botao;
+    }
+
+    public static function botaoExcluirConfirm($id, $title = 'Excluir')
+    {
+        $attributos = ['class' => 'btn-excluir btn-xs', 'id' => 'botaoexcluir'];
+        return self::botao($id, 'glyphicon glyphicon-remove-sign', $attributos, $title);
+    }
+
+    public static function botaoNovo($url, $title = 'Novo')
+    {
+        $attributos = ['class' => 'btn-novo btn-xs', 'id' => 'botaonovo'];
         return self::botaoLink($url, 'glyphicon glyphicon-remove-sign', $attributos, $title);
     }
 
-    public static function botaoExcluir($url, $title='Excluir'){
-        $attributos = ['class'=>'btn-excluir btn-xs', 'id'=>'botaoexcluir'];
+    public static function botaoExcluir($url, $title = 'Excluir')
+    {
+        $attributos = ['class' => 'btn-excluir btn-xs', 'id' => 'botaoexcluir'];
         return self::botaoLink($url, 'glyphicon glyphicon-remove-sign', $attributos, $title);
     }
 
-    public static function botaoDesativar($url, $title='Desativar', $icone_bootstrap = 'glyphicon glyphicon-off', $id = 'botaodesativar'){
-        $attributos = ['class'=>'btn-desativar btn-xs', 'id'=>$id];
+    public static function botaoDesativar($url, $title = 'Desativar', $icone_bootstrap = 'glyphicon glyphicon-off', $id = 'botaodesativar')
+    {
+        $attributos = ['class' => 'btn-desativar btn-xs', 'id' => $id];
         return self::botaoLink($url, $icone_bootstrap, $attributos, $title);
     }
 
-    public static function botaoAlterar($url, $title='Alterar'){
-        $attributos = ['class'=>'btn-alterar btn-xs', 'id'=>'botaoalterar'];
+    public static function botaoAlterar($url, $title = 'Alterar')
+    {
+        $attributos = ['class' => 'btn-alterar btn-xs', 'id' => 'botaoalterar'];
         return self::botaoLink($url, 'glyphicon glyphicon-edit', $attributos, $title);
     }
 
-    public static function botaoAlterarCustomizado($url, $title='Modificar'){
-        $attributos = ['class'=>'btn-alterar btn-xs btn-alterar-customizado', 'id'=>'botaomodificar'];
+    public static function botaoAlterarCustomizado($url, $title = 'Modificar')
+    {
+        $attributos = ['class' => 'btn-alterar btn-xs btn-alterar-customizado', 'id' => 'botaomodificar'];
         return self::botaoLink($url, 'glyphicon glyphicon-user', $attributos, $title);
     }
 
-    public static function botaoAcao($url, $title='A��o', $id = 'botaoacao', $icone_bootstrap = 'glyphicon glyphicon-screenshot', $classe = 'btn btn-default'){
-        $attributos = ['class'=>$classe, 'id'=>$id];
+    public static function botaoAcao($url, $title = 'A��o', $id = 'botaoacao', $icone_bootstrap = 'glyphicon glyphicon-screenshot', $classe = 'btn btn-default')
+    {
+        $attributos = ['class' => $classe, 'id' => $id];
         #$attributos = ['class'=>'btn-alterar btn-xs btn-alterar-customizado', 'id'=>$id];
         return self::botaoLink($url, $icone_bootstrap, $attributos, $title);
     }
 
-    public static function botaoDownload($url, $title='A��o', $id = 'botaoacao', $icone_bootstrap = 'glyphicon glyphicon-screenshot', $classe = 'btn btn-default'){
-        $attributos = ['class'=>$classe, 'id'=>$id];
+    public static function botaoDownload($url, $title = 'A��o', $id = 'botaoacao', $icone_bootstrap = 'glyphicon glyphicon-screenshot', $classe = 'btn btn-default')
+    {
+        $attributos = ['class' => $classe, 'id' => $id];
         #$attributos = ['class'=>'btn-alterar btn-xs btn-alterar-customizado', 'id'=>$id];
         return self::botaoLink($url, $icone_bootstrap, $attributos, $title, " target='_blank'");
     }
 
-    private static function arrayToString($array){
+    private static function arrayToString($array)
+    {
         $string = '';
-        foreach($array as $chave => $item){
+        foreach ($array as $chave => $item) {
             $string .= "{$chave}='{$item}' ";
         }
         return $string;
