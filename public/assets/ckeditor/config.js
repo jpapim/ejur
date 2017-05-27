a2a1b777c0a4447603597e075b253eafe0dc0fa2
EduFerr <eduardofgsantos@gmail.com>
2017-05-27 00:56:27 -0300

    melhorando layout no módulo questão

--- public/assets/ckeditor/config.js
+++ public/assets/ckeditor/config.js
@@ -10,25 +10,26 @@
         
       CKEDITOR.editorConfig = function( config ) {
 	config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
+		// { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		// { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		// { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		// { name: 'forms', groups: [ 'forms' ] },
 		'/',
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
+		// { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		// { name: 'links', groups: [ 'links' ] },
 		{ name: 'insert', groups: [ 'insert' ] },
 		'/',
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
+		// { name: 'styles', groups: [ 'styles' ] },
+		// { name: 'colors', groups: [ 'colors' ] },
+		// { name: 'tools', groups: [ 'tools' ] },
+		// { name: 'others', groups: [ 'others' ] },
+		// { name: 'about', groups: [ 'about' ] }
 	];
 
-	config.removeButtons = 'Save,NewPage,Preview,Print,Source,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Redo,Undo,Find,Smiley,PageBreak,Iframe,ShowBlocks,About,Superscript,Subscript,CreateDiv,Link,Unlink,Anchor,Flash,SelectAll,Select,Textarea,TextField,Radio,Checkbox,HiddenField,ImageButton,Button,Form,Replace,Language';
-};
+	config.removeButtons = 'HorizontalRule,SpecialChar,Strike,Table,Save,NewPage,Preview,Print,Source,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Redo,Undo,Find,Smiley,PageBreak,Iframe,ShowBlocks,About,Superscript,Subscript,CreateDiv,Link,Unlink,Anchor,Flash,SelectAll,Select,Textarea,TextField,Radio,Checkbox,HiddenField,ImageButton,Button,Form,Replace,Language';
+
+      };
         
         
         
