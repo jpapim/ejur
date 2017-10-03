ccd95acbfe567b684f58636784e7d367efa6ce01
Mário <mariosotnas@gmail.com>
2017-10-03 16:11:24 -0300

    [UPDATE] Adicionando paginação no módulo FiltroProva (em andamento)

--- data/db/alteracao/mario/alteracao_20170930.sql
+++ data/db/alteracao/mario/alteracao_20170930.sql
@@ -1,2 +1,2 @@
 -- Adiciona a coluna nm_filtro_prova para armazenar o nome do filtro pre definido
-alter table filtro_prova add column nm_filtro_prova varchar(45) COMMENT 'Campo utilizado para armazenar o nome do filtro já pre definido';
+alter table filtro_prova add column nm_filtro_prova varchar(45) COMMENT 'Campo utilizado para armazenar o nome do filtro já pré definido';
